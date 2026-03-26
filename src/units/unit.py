db3e8e6bf78225b0fc11dff943a80f45c39fd569
Thomas <mathwo37@outlook.fr>
2026-03-26 09:32:55 +0100

    commencer le systeme d'unit et la recherche du bon fichier

--- src/units/unit.py
+++ src/units/unit.py
@@ -1,11 +1,19 @@
 import os
+import json
 
 class Unit:
-    def __init__(self):
+    def __init__(self,name):
         list_files=os.listdir("definitions")
         for file in list_files:
-            if file.endswith(".json"):
-                self.fichier=list_files.index(file)
+            if file.startswith(name):
+                self.fichier=file
+        with open(self.fichier, 'r', encoding='utf-8'):
+            donnees = json.load(str(self.fichier))
 
-test1=Unit()
+        # Afficher les données pour vérifier le contenu
+        print(donnees)
+
+test1=Unit("pekka")
+test2=Unit("esprit_de_glace")
 print(test1.fichier)
+print(test2.fichier)
