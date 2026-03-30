54ec1340f82079290b6ad08bc6bab41738ed8d84
Thomas <mathwo37@outlook.fr>
2026-03-30 15:56:19 +0200

    add many(very very many) cards and stats
    Signed-off-by: Thomas <mathwo37@outlook.fr>

--- src/units/unit.py
+++ src/units/unit.py
@@ -1,19 +1,20 @@
-import os
 import json
+import os
+
 
 class Unit:
     def __init__(self,name):
         list_files=os.listdir("definitions")
         for file in list_files:
             if file.startswith(name):
-                self.fichier=file
-        with open(self.fichier, 'r', encoding='utf-8'):
-            donnees = json.load(str(self.fichier))
+                self.file = file
+        with open(self.file, 'r', encoding='utf-8'):
+            data = json.load(self.file)
 
-        # Afficher les données pour vérifier le contenu
-        print(donnees)
+            # Afficher les données pour vérifier le contenu
+            print(data)
 
 test1=Unit("pekka")
 test2=Unit("esprit_de_glace")
-print(test1.fichier)
-print(test2.fichier)
+print(test1.file)
+print(test2.file)
