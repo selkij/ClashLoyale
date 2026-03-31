13afc7948845682d6292c60960a78b46ac815345
Lapruche <benjbde1@gmail.com>
2026-03-31 09:49:01 +0200

    CRITIQUES

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ from utils import log
 
 def run():
     log.Logger('debug.log', logging.DEBUG)  # Will directly populate the logger variable
-
+    
     # Pygame init
     pygame.init()
     pygame.font.init()
@@ -35,13 +35,16 @@ if __name__ == "__main__":
     try:
         run()
     except Exception:
-        log.logger.send("// I'm sorry, dave. I'm afraid I can't do that.", logging.CRITICAL)
+        log.logger.send("----------------------CRITIQUES---------------------", logging.CRITICAL)
+        log.logger.send("// I'm sorry, dave. I'm afraid I can't do that. HOG RIIIIDAAAAH\n", logging.CRITICAL)
         exc_type, exc_value, exc_traceback = sys.exc_info()
         tb_list = traceback.format_exception(exc_type, exc_value, exc_traceback)
 
         for line in tb_list:
             clean_line = line.replace("\n", "")
             log.logger.send(clean_line, logging.CRITICAL)
+
+        log.logger.send("----------------------------------------------------\n", logging.CRITICAL)
 
         pygame.quit()
         sys.exit(-1)
