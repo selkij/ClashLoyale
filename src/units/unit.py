7b982726a477b8831cd96874fe4a0b42049b311e
draco <ladressebordel@outlook.fr>
2026-04-02 09:19:08 +0200

    reconize if it's a valid cards and can extract data from the json file

--- src/units/unit.py
+++ src/units/unit.py
@@ -5,16 +5,20 @@ import os
 class Unit:
     def __init__(self,name):
         list_files=os.listdir("definitions")
+        self.file=None
         for file in list_files:
             if file.startswith(name):
                 self.file = file
-        with open(self.file, 'r', encoding='utf-8'):
-            data = json.load(self.file)
+        if self.file==None:
+            raise ValueError("File not found:",name)
+        else:
+            print("File found success:",name)
+        data=json.load(open("definitions/"+self.file))
+        print(data)
+
+
 
             # Afficher les données pour vérifier le contenu
-            print(data)
 
 test1=Unit("pekka")
-test2=Unit("esprit_de_glace")
-print(test1.file)
-print(test2.file)
+test2=Unit("mini_pekka")
